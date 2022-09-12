d03ed9a90ad4023d40fc6ed00c6efc12b2f9dd51
Bolade Dennis <you@example.com>
2022-09-12 11:34:34 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: reserve alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'Z'; i >= 'A'; i--)
+
+	{
+		printf("%c", i);
+	}
+	return (0);
+}
