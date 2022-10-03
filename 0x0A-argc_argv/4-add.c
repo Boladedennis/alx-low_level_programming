09eb06f49478839ea54a800875b29abd0cf72af2
Bolade Dennis <you@example.com>
2022-10-03 00:48:22 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
+/**
+ * isInteger - checks if s is an integer
+ * @s: string to check
+ * Return: 0 or 1
+ */
+
+int isInteger(const char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
+		return (1);
+	i++;
+}
+return (0);
+}
 
 /**
- * main -Entry point
- * Description: a program that adds positive numbers
- * If no number is passed to the program, print 0, followed by a new line
- * If one of the number contains symbols that are not digits, print Error, 
- * followed by a new line, and return 1
- *
- * @argc: argument count
- *
- * @argv: argument vector (arrays of string)
- *
- * Return: always 0
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
-int main(int argc, char *argv[])
-{
-	int i, sum;
 
-	sum = 0;
-	for (i = 1; i < argc; i++)
+int main(int argc, char const *argv[])
+{
+int sum = 0;
+while (--argc)
+{
+	if (isInteger(argv[argc]))
 	{
-		if (argc > 1)
-		{
-			sum += atoi(argv[i]);
-		}
-		else if (argc <= 1)
-		{
-			printf("0\n");
-		}
-		else (!isdigit(argv[i]))
-		{
-			printf("Error\n");
-			return (1);
-		}
+		printf("Error\n");
+		return (1);
 	}
-	printf("%d\n", sum);
-	return (0);
+	sum += atoi(argv[argc]);
+}
+
+printf("%i\n", sum);
+
+return (0);
 }
